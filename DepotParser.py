077e0b4b04034507b078b28e8f7836fe10bd8b23
Diceycle <30533536+Diceycle@users.noreply.github.com>
2023-03-10 15:19:19 +0100

    fix parser breaking when depot cant scroll a full page

--- DepotParser.py
+++ DepotParser.py
@@ -1,4 +1,5 @@
 import os
+import time
 
 from WindowHandler import WindowHandler
 from config import LOGGER
@@ -32,12 +33,21 @@ MAIN_MENU_CHECKS = [
 DEPOT_BUTTON_SIZE = 250
 
 FIRST_MATERIAL_CENTER = (160, 240)
+FIRST_MATERIAL_CENTER_END = (269, 240)
 MATERIAL_DISTANCE = (199, 243)
 BOX_RADIUS = 120
 BOXES_ON_SCREEN = (7, 3)
 
 SCROLL_LINE_START = (1450, 360)
 SCROLL_LINE_END = (25, 360)
+
+DEPOT_END_CHECKS = [
+    (1560, 160),
+    (1560, 210),
+    (1560, 250),
+    (1560, 290),
+    (1560, 330)
+]
 
 AMOUNT_CROP_BOX = (118, 157, 181, 192)
 AMOUNT_CROP_BOX_SLIM = (123, 163, 176, 188)
@@ -65,13 +75,23 @@ def takeScreenshot(windowHandler):
 def scrollArknights(windowHandler):
     resizeArknights(windowHandler)
     windowHandler.dragLine(SCROLL_LINE_START, SCROLL_LINE_END)
+    # Give Arknights time to snap back in case of overscroll
+    time.sleep(1)
 
-def splitScreenshot(screenshot):
+def checkEndOfDepot(image):
+    for p in DEPOT_END_CHECKS:
+        pix = image.getpixel(p)
+        if pix[0] < 200 or pix[1] < 200 or pix[2] < 200:
+            return False
+
+    return True
+
+def splitScreenshot(screenshot, firstBoxPosition):
     boxes = []
     for x in range(BOXES_ON_SCREEN[0]):
         for y in range(BOXES_ON_SCREEN[1]):
-            posX = FIRST_MATERIAL_CENTER[0] + x * MATERIAL_DISTANCE[0]
-            posY = FIRST_MATERIAL_CENTER[1] + y * MATERIAL_DISTANCE[1]
+            posX = firstBoxPosition[0] + x * MATERIAL_DISTANCE[0]
+            posY = firstBoxPosition[1] + y * MATERIAL_DISTANCE[1]
             boxes.append(screenshot.crop((posX - BOX_RADIUS, posY - BOX_RADIUS,
                                           posX + BOX_RADIUS, posY + BOX_RADIUS)))
 
@@ -144,8 +164,7 @@ class DepotParser:
             return "Arknights is not running or cannot be found. Check your Arknights Window configuration."
 
         self.materialIndex = 0
-        self.boxIndex = 0
-        self.scrollNotified = False
+        self.finalPage = False
 
         if self.handler is not None:
             screenshot = validateMenu(self.handler)
@@ -155,17 +174,38 @@ class DepotParser:
         if screenshot is None:
             return "Can not scan depot from here, please navigate to the main menu or the depot."
 
-        self.boxes = splitScreenshot(screenshot)
+        self.loadNextPage(screenshot, firstPage=True)
+
         return None
+
+    def loadNextPage(self, screenshot, firstPage = False):
+        self.scrollNotified = False
+        if checkEndOfDepot(screenshot):
+            self.finalPage = True
+            self.boxes = splitScreenshot(screenshot, FIRST_MATERIAL_CENTER_END)
+            if not firstPage:
+                self.boxIndex = self.findFirstUnknownBox()
+            else:
+                self.boxIndex = 0
+        else:
+            self.boxes = splitScreenshot(screenshot, FIRST_MATERIAL_CENTER)
+            self.boxIndex = 0
+        self.lastTopRow = [None for i in range(BOXES_ON_SCREEN[0])]
+
+    def findFirstUnknownBox(self):
+        c = 0
+        for m in self.lastTopRow:
+            if matchMasked(self.boxes[0], m) > self.confidenceThreshold:
+                return (BOXES_ON_SCREEN[0] - c) * BOXES_ON_SCREEN[1]
+            c += 1
+        return 0
 
     def parseNext(self):
         if self.boxIndex >= len(self.boxes):
             if self.handler is not None and self.scrollNotified:
-                self.scrollNotified = False
                 scrollArknights(self.handler)
-                self.boxes = splitScreenshot(takeScreenshot(self.handler))
-                self.boxIndex = 0
-            elif self.handler is not None:
+                self.loadNextPage(takeScreenshot(self.handler))
+            elif self.handler is not None and not self.finalPage:
                 self.scrollNotified = True
                 return "Scrolling..."
             else:
@@ -189,12 +229,14 @@ class DepotParser:
         return mat, amount
 
     def parseMaterialAmount(self):
-        material = DEPOT_ORDER[self.materialIndex]
+        material = MATERIALS[DEPOT_ORDER[self.materialIndex]]
         box = self.boxes[self.boxIndex]
 
-        confidence = matchMasked(box, MATERIALS[material])
+        confidence = matchMasked(box, material)
         if confidence > self.confidenceThreshold:
             amount = readAmount(box, debug=self.debug, fileName=self.materialIndex)
+            if self.boxIndex % BOXES_ON_SCREEN[1] == 0:
+                self.lastTopRow[self.boxIndex // BOXES_ON_SCREEN[1]] = material
             self.boxIndex += 1
         else:
             amount = None
@@ -202,11 +244,11 @@ class DepotParser:
         if self.expectedResult is not None:
             if self.expectedResult[self.materialIndex] != amount:
                 print("!!! Material at Position {:>2}: {:>16} x{:<4} but should be {}".format(
-                    self.boxIndex, material, amount, self.expectedResult[self.materialIndex]))
+                    self.boxIndex, material.name, amount, self.expectedResult[self.materialIndex]))
             else:
-                print("    Material at Position {:>2}: {:>16} x{:<4}".format(self.boxIndex, material, amount))
+                print("    Material at Position {:>2}: {:>16} x{:<4}".format(self.boxIndex, material.name, amount))
 
-        return MATERIALS[material], amount
+        return material, amount
 
     def destroy(self):
         if self.handler is not None:
